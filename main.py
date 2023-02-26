768d86d78b8524db35d0b1cddcaff26d46519639
Ogannes <Vovan91146@mail.ru>
2023-02-26 23:47:18 +0300

    Прорешал задачи с семинара

--- main.py
+++ main.py
@@ -33,12 +33,44 @@
 
 # print(text.replace('Давай', 'ДаВаЙ')) # Меняет сочетание символов в строке
 
-text = 'Давай ПоУчиМ Python'
-print(text[0])  # Д
-print(text[1])  # а
-print(text[len(text)-1])  # Выведет n (последняя буква)
-print(text[-1])  # выведет первый элемент с конца - n
-print(text[:])  # вывод text
-print(text[:2]) # Вывод элементов с 0 до 2-го т.е. Да
-print(text[len(text)-2:])
-print(text[2:8]) # Вывод элементов с 2 до 9-го т.е. Да
+# text = 'Давай ПоУчиМ Python'
+# print(text[0])  # Д
+# print(text[1])  # а
+# print(text[len(text)-1])  # Выведет n (последняя буква)
+# print(text[-1])  # выведет первый элемент с конца - n
+# print(text[:])  # вывод text
+# print(text[:2]) # Вывод элементов с 0 до 2-го т.е. Да
+# print(text[len(text)-2:])
+# print(text[2:8]) # Вывод элементов с 2 до 9-го т.е. Да
+
+# list_1 = [1, 2, 3, 4, 5]
+# print(list_1.pop()) #5
+# print(list_1)
+# print(list_1.pop())
+# print(list_1)
+# print(list_1.pop())
+# print(list_1)
+# print(list_1.pop())
+# print(list_1)
+# print(list_1.pop(0))
+# print(list_1)
+
+# list_1 = [1, 2, 3, 4, 5]
+# print(list_1.insert(2, 11)) # где 1-е число это индекс, а 2-е это 
+# print(list_1)
+
+# t = ()
+# print(t)
+
+# t= (1, 2, 3)
+# print(type(t))
+
+# list_1 = []
+# for i in range (1,101):
+#     if i%2==0:
+#         list_1.append(i)
+# print(list_1)
+
+# list_1 = [i for i in range (1,101) if i%2==0]
+# print(list_1)
+
